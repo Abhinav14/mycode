323dd4acf61a54314f4995c3e35017d3f7badb9a
Abhinav14 <abhinav.anant14@gmail.com>
2023-01-17 05:28:10 +0000

    added hmax.txt, smondo.xml and modefied New.java

--- New.java
+++ New.java
@@ -1,5 +1,6 @@
 public class New{
 	public static void main(String[] args){
 		System.out.println("This is a test file for Git");
+		System.out.println("Added another line for version control");
 	}
 }
